f4378f5ed4cc82e109da8e66f97f60abe1552573
heuguchon <heuguchon@wiselight.kr>
2023-10-27 13:29:52 +0900

    style: 클라이언트 아이콘 변경

--- indra/newview/linux_tools/refresh_desktop_app_entry.sh
+++ indra/newview/linux_tools/refresh_desktop_app_entry.sh
@@ -14,18 +14,18 @@ function install_desktop_entry()
 [Desktop Entry]\n\
 Name=Heuguchon Client\n\
 Comment=Client for accessing 3D virtual worlds\n\
-Exec=${installation_prefix}/firestorm\n\
-Icon=${installation_prefix}/firestorm_icon.png\n\
+Exec=${installation_prefix}/heuguchon\n\
+Icon=${installation_prefix}/heuguchon_icon.png\n\
 Terminal=false\n\
 Type=Application\n\
 Categories=Application;Internet;Network;\n\
 StartupNotify=true\n\
 X-Desktop-File-Install-Version=3.0\n\
-StartupWMClass=do-not-directly-run-firestorm-bin"
+StartupWMClass=do-not-directly-run-heuguchon-bin"
 
     echo " - Installing menu entries in ${desktop_entries_dir}"
     mkdir -vp "${desktop_entries_dir}"
-    echo -e $desktop_entry > "${desktop_entries_dir}/firestorm-viewer.desktop" || "Failed to install application menu!"
+    echo -e $desktop_entry > "${desktop_entries_dir}/heuguchon.desktop" || "Failed to install application menu!"
 }
 
 if [ "$UID" == "0" ]; then
