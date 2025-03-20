674000e17f3fa58699ecd8c69f337441f363ca79
heuguchon <heuguchon@wiselight.kr>
2025-03-20 18:08:37 +0900

    fix: macOS 빌드시 Firestorm.nib를 찾는 문제 해결

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -1544,7 +1544,7 @@ class Darwin_x86_64_Manifest(ViewerManifest):
                 with self.prefix(src=icon_path) :
                     self.path("heuguchon_icon.icns")
 
-                self.path("Firestorm.nib")
+                self.path("Heuguchon.nib")
                 # Translations
                 self.path("English.lproj/language.txt")
                 self.replace_in(src="English.lproj/InfoPlist.strings",
